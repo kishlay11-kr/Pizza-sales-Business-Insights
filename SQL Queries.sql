ade1e83ad611eb07e6c31531eda9136e81e341a9
kishlay11-kr <kumarkishlay987@gmail.com>
2026-03-01 16:26:57 +0530

    Update and rename SQL Queries to SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -14,4 +14,18 @@ from pizza_sales
 
 select * from pizza_sales
 
+-- daily/monthly trends--
+
+select * from pizza_sales
+
+select DATENAME(DW, order_date) as order_day, count(distinct order_id) as Total_orders
+from pizza_sales
+group by DATENAME(DW,order_date)
+
+select DATENAME(month, order_date) as order_day, count(distinct order_id) as Total_orders
+from pizza_sales
+group by DATENAME(month,order_date)
+order by Total_orders desc
+
+
 
